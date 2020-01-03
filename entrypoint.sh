ba96173b9ada51d32ecf395df017dda7e9216e8a
Ed Shelton <edmund.shelton@imaginecurve.com>
2020-01-03 16:32:23 +0000

    fix terrible bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,33 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
 echo "======================"
 echo "= Linting YAML files ="
 echo "======================"
 
-if [ -z "INPUT_FILE_OR_DIR" ]; then
+if [ -z "$INPUT_FILE_OR_DIR" ]; then
     echo "file_or_dir not provided, add it in workflow"
     exit 1
 fi
 
 STRICT=""
 if [ "$INPUT_STRICT" == "true" ]; then
-    STRICT="-s"
+  STRICT="-s"
 fi
 
-if [ ! -z "$INPUT_CONFIG_FILE" ]; then
-    CONFIG_FILE="-c $INPUT_CONFIG_FILE"
+if [ -n "$INPUT_CONFIG_FILE" ]; then
+  CONFIG_FILE="-c $INPUT_CONFIG_FILE"
 fi
 
-if [ ! -z "$INPUT_CONFIG_DATA" ]; then
-    CONFIG_DATA="-d $INPUT_CONFIG_DATA"
+if [ -n "$INPUT_CONFIG_DATA" ]; then
+  CONFIG_DATA="-d $INPUT_CONFIG_DATA"
 fi
 
-yamllint $CONFIG_FILE $CONFIG_DATA -f $INPUT_FORMAT $STRICT $INPUT_FILE_OR_DIR 
+if [[ -n "$INPUT_NO_WARNINGS" && "$INPUT_NO_WARNINGS" != "false" ]]; then
+  NO_WARNINGS="--no-warnings"
+fi
+
+if [ -n "$INPUT_FORMAT" ]; then
+  FORMAT="-f $INPUT_FORMAT"
+fi
+
+yamllint $CONFIG_FILE $CONFIG_DATA $FORMAT $STRICT $NO_WARNINGS $INPUT_FILE_OR_DIR
